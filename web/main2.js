0825454e5f63f3bb244b6801aa5820b8a8359d55
chema <chemamengibar@gmail.com>
2023-11-17 05:58:21 +0100

    add: 1

---
+++ web/main2.js
@@ -0,0 +1,196 @@
+import * as THREE from 'three';
+import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
+import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
+
+import './style.scss'
+
+const scene = new THREE.Scene();
+
+const camConfig = {
+    fov: 75,
+    aspect: (window.innerWidth/2) / window.innerHeight,
+    near: 0.1,
+    far: 1000.0
+}
+
+
+const camera = new THREE.PerspectiveCamera( camConfig.fov, camConfig.aspect , camConfig.near, camConfig.far );
+
+scene.add(camera);
+
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize( window.innerWidth / 2 , window.innerHeight );
+document.body.appendChild( renderer.domElement );
+
+// AXIS HELPER
+const axesHelper = new THREE.AxesHelper( 5 );
+scene.add( axesHelper );
+
+
+
+// ######################################################################### LIGHT
+const light = new THREE.AmbientLight( 0xffffff );
+light.position.set(10, 10, 20)
+light.intensity = 5
+light.castShadow = true;
+scene.add(light)
+
+// ######################################################################### 3D
+const loader = new GLTFLoader();
+loader.load( 'rink-base.glb', function ( gltf ) {
+     scene.add( gltf.scene );
+
+}, undefined, function ( error ) {
+    console.error( '>>>>>>>>', error );
+} );
+
+
+// ######################################################################### CONTROLS
+//const controls = new OrbitControls( camera, renderer.domElement );
+// controls.object.position.set(5, 1, 0);
+// controls.target = new THREE.Vector3(-6, 0, -6);
+
+// controls.maxPolarAngle = Math.PI / 2.2;
+// controls.minPolarAngle = Math.PI / 2.2;
+
+// controls.object.position.set(1, 1,  5);
+// controls.target = new THREE.Vector3(5, 1, 10);
+
+// controls.enableZoom = false;
+
+
+// ######################################################################### CAMERA
+
+camera.position.set(0,1,0);
+
+
+
+const pt = new THREE.Vector3(0,1,2)
+camera.lookAt(pt);
+
+
+
+
+
+
+
+
+// ##########################################################################  OBJECTS
+
+// ------------------------------------------ BOX
+const geometry = new THREE.BoxGeometry( 1, 1, 0.1 );
+const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+const cube = new THREE.Mesh( geometry, material );
+cube.userData.id ="CUBE"
+
+scene.add( cube );
+
+
+const xMax = 12.5;
+const xMin = -xMax;
+
+const yMax = 30;
+const yMin = -yMax;
+
+// 30:13, 29:8,  28:10, 27:10
+
+cube.position.x = 27;
+cube.position.z = 10;
+cube.position.y = 0;
+
+let angle = 90;
+let radians = 2 * Math.PI * (angle / 360);
+cube.rotation.y =  radians;
+
+// ------------------------------------------ TRIANGLE
+let vertices = new Float32Array([
+    0.0, 0.0, 0.0,    // vertex 1
+    0.0, 2.0, 0.0,     // vertex 2
+    0.0, 0.0, 2.0,      // vertex 3
+]);
+
+const  a = new THREE.BufferGeometry();
+a.setAttribute('position', new THREE.BufferAttribute(vertices, 3));
+
+const m = new THREE.MeshBasicMaterial({ color: 'red' })
+m.side = THREE.DoubleSide;
+const d = new THREE.Mesh(a, m);
+
+
+let angle1 = 220;
+let radians1 = 2 * Math.PI * (angle1 / 360);
+d.rotation.y =  radians1;
+d.userData.id = "TRIANGLE"
+scene.add(d)
+
+
+
+
+// ---------------------------------------------------------------------------------------------------- EVENTS 
+
+// KEYs
+var delta = 0;
+var h = 0;
+
+function onDocumentKeyDown(event){
+    delta = 0.01;
+    h = 0.02;
+    event = event || window.event;
+    var keycode = event.keyCode;
+    console.log(keycode)
+
+    let angleRot = 5;
+    let radiansRot = 2 * Math.PI * (angleRot / 360);
+    const pt = new THREE.Vector3(0,0,0)
+
+    switch(keycode){
+        case 37 :
+            camera.rotation.y = camera.rotation.y + radiansRot;
+            break;
+        case 39 :
+            camera.rotation.y = camera.rotation.y - radiansRot;
+            break;
+        // case 38 :
+        //     camera.rotation.x = camera.rotation.x + h;
+        //     break;
+        // case 40 :
+        //     camera.rotation.x = camera.rotation.x - h;
+        //     break;
+    }
+}
+
+document.addEventListener('keydown',onDocumentKeyDown,false);
+
+
+// ---------
+
+const raycaster = new THREE.Raycaster();
+const pointer = new THREE.Vector2();
+
+
+window.addEventListener('mousedown', onTouchCallback)
+
+function onTouchCallback(event) {
+    pointer.x = (event.clientX / window.innerWidth) * 2 - 1;
+    pointer.y = - (event.clientY / window.innerHeight) * 2 + 1;
+
+    raycaster.setFromCamera(pointer, camera);
+
+    const intersects = raycaster.intersectObjects(scene.children);
+
+    for (let i = 0; i < intersects.length; i++) {
+        const objId = intersects[i].object.userData.id
+        console.log(objId)
+    }
+
+}
+
+
+function animate() {
+    requestAnimationFrame(animate)
+    renderer.render(scene, camera)
+}
+
+
+
+animate()
